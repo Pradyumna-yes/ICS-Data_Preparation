499380dfeb30406dbf8c15c34749b6309c3662ab
Pradyumna S <96283346+Pradyumna-yes@users.noreply.github.com>
2025-03-20 14:04:39 +0000

    Delete db.py

--- db.py
+++
@@ -1,6 +0,0 @@
-import pyodbc
-conn_str = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=<CRMServer3>;DATABASE=<CRM_MSCRM>"
-conn = pyodbc.connect(conn_str)
-cursor = conn.cursor()
-cursor.execute("SELECT @@VERSION")
-print(cursor.fetchone())
